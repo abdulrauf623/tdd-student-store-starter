e8946ae79765a81524e2f8cde20dcbe26a86c12d
Abdul Rauf Abdul Karim <abdul.karim@abdulka-ltmat6w.internal.salesforce.com>
2022-06-30 13:37:13 -0700

    API call works for the get and post requests

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,43 @@
 // YOUR CODE HERE
+
+
+const express = require("express")
+
+
+const cors = require("cors")
+
+const morgan = require('morgan')
+
+const app = express()
+app.use(cors())
+
+const store = require("/Users/abdul.karim/studentStorePROJECT/models/Store")
+
+app.use(morgan("tiny"))
+
+
+const stores = require("/Users/abdul.karim/studentStorePROJECT/routes/store")
+
+
+app.use(express.json())
+
+
+
+app.use("/store", stores)
+
+
+
+
+
+
+
+app.get("/", async(req, res, next) => {
+
+    
+
+
+    res.status(200).json({ping : "pong"})
+})
+
+
+module.exports = app
